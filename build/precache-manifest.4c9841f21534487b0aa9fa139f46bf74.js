43cec7e1e4274c364fae99db70cc626c025017b5
Levonhar930 <levonhar93@mail.ru>
2020-06-12 21:23:05 +0400

    build

---
+++ build/precache-manifest.4c9841f21534487b0aa9fa139f46bf74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61cd76e0760333777d7d493a17703d1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df7be9904856f159a0fd",
+    "url": "/static/css/main.5e1b1b1c.chunk.css"
+  },
+  {
+    "revision": "6b192293ea757fdb4781",
+    "url": "/static/js/2.ab6c85de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ab6c85de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df7be9904856f159a0fd",
+    "url": "/static/js/main.104bc937.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "5c0436d55f12af99760ca65640d19d19",
+    "url": "/static/media/logo.5c0436d5.png"
+  }
+]);
